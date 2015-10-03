f4d01ea5c2fce1afbfd050d99c9c5d57a1360783
Leo Carbajal <desleo@gmail.com>
2015-10-02 21:27:28 -0500

    Fixed a couple of small things with styling and missing deps.

--- app/pods/recipes/route.js
+++ app/pods/recipes/route.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return new Ember.RSVP.Promise(function(resolve) {
-      setTimeout(3000, resolve);
+      setTimeout(resolve, 3000);
     });
   },
 });
